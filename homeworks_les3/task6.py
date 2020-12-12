2f218b3a6690a36c2454463f0489f1c6a7724df4
galin0ff <g4linoff@gmail.com>
2020-12-12 22:15:15 +0700

    All homeworks lesson 3

--- homeworks_les3/task6.py
+++ homeworks_les3/task6.py
@@ -1,13 +1,27 @@
 
 
-def int_func(sent_t: str):
-    num_ord = ord(sent_t[0])
-    num_char = chr((num_ord) + 32)
-    sent_t[0] = num_char
-    return sent_t
+def int_func(sentence: str) -> str:
+    first_seentence
+    result = list(sentence)
+    char_change = ord(result[0]) - 32
+    char_change = chr(char_change)
+    result[0] = char_change
+    return ''.join(result)
 
 
-sent = input('Введите предложение')
-print(int_func(sent))
+user_str = input('Введите строку: \n')
+print(int_func(user_str))
 
-
+# user_str = input('Введите строку: \n')
+#
+# print(ord('t'))
+# print(chr(116))
+#
+# print(ord(user_str[0]))
+# char_change = ord(user_str[0]) - 32
+# print(char_change)
+# num_change = chr(char_change)
+# print(num_change)
+# user_str_list = list(user_str)
+# user_str_list[0] = num_change
+# print(''.join(user_str_list))
